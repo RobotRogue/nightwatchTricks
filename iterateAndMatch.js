4c448c1454824f2ce5a7cac5ea1eaf2793b52919
Nelson Gill <RobotRogue@users.noreply.github.com>
2018-05-04 14:21:33 -0700

    Refactor and add TODO

--- iterateAndMatch.js
+++ iterateAndMatch.js
@@ -1,25 +1,24 @@
 // Call function from test as findMatch(browser, randomDonation);
 // randomDonation is a generated variable within the test, stored as a string
+// Intent is to find a match, and its corresponding TR index, and then do the following:
+// click('tr:nth-child(' + indexPosition + ') .delete-recurring-money-transfer');
 
 function findMatch(browser, randomDonation) {
-
     var tableDataToCompare;
-    var counter = 1;
     var comparatorValue = "$" + randomDonation;
-    browser.elements('css selector', '#recurring-gifts-table td', function (result) {
+    var indexPosition = 0;
+    browser.elements('css selector', '#recurring-gifts-table tr', function (result) {
         if (result.value) {
             tableDataToCompare = result.value;
             tableDataToCompare.forEach(function (cellValue) {
-                counter++;
                 browser.elementIdText(cellValue.ELEMENT, function (cellValueText) {
-                    if (cellValueText.value == comparatorValue) {
-                        console.log("Found match of: " + comparatorValue + " to... " + cellValueText.value);
-                        console.log(counter); // Ideally this is the <td> index where the data was matched. Untested.
-                        counter++;
+                    if (cellValueText.value.includes(comparatorValue)) {
+                        console.log("##### Found match of: " + comparatorValue + " to... " + cellValueText.value + " at position " + indexPosition + " #####");
+                        return (indexPosition); // TODO: HOW DO I EXPORT THIS AS A VARIABLE THAT MY TEST CAN USE?
                     } else {
-                        console.log(counter);
-                        counter++;
+                        console.log("##### NO MATCH FOUND!! #####");
                     }
+                    indexPosition++;
                 })
             })
         }
